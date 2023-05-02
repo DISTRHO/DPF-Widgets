fc8750dc9f6aab97beac9226a24bf9212e9b4595
dreamer <dreamer@puikheid.nl>
2023-05-02 13:41:52 +0200

    disable resize, collapse and position for demo

--- opengl/DearImGui/imgui_demo.cpp
+++ opengl/DearImGui/imgui_demo.cpp
@@ -305,8 +305,8 @@ void ImGui::ShowDemoWindow(bool* p_open)
     static bool no_scrollbar = false;
     static bool no_menu = false;
     static bool no_move = false;
-    static bool no_resize = false;
-    static bool no_collapse = false;
+    static bool no_resize = true;
+    static bool no_collapse = true;
     static bool no_close = false;
     static bool no_nav = false;
     static bool no_background = false;
@@ -326,11 +326,11 @@ void ImGui::ShowDemoWindow(bool* p_open)
     if (unsaved_document)   window_flags |= ImGuiWindowFlags_UnsavedDocument;
     if (no_close)           p_open = NULL; // Don't pass our bool* to Begin
 
-    // We specify a default position/size in case there's no data in the .ini file.
-    // We only do it to make the demo applications a little more welcoming, but typically this isn't required.
-    const ImGuiViewport* main_viewport = ImGui::GetMainViewport();
-    ImGui::SetNextWindowPos(ImVec2(main_viewport->WorkPos.x + 650, main_viewport->WorkPos.y + 20), ImGuiCond_FirstUseEver);
-    ImGui::SetNextWindowSize(ImVec2(550, 680), ImGuiCond_FirstUseEver);
+    // // We specify a default position/size in case there's no data in the .ini file.
+    // // We only do it to make the demo applications a little more welcoming, but typically this isn't required.
+    // const ImGuiViewport* main_viewport = ImGui::GetMainViewport();
+    // ImGui::SetNextWindowPos(ImVec2(main_viewport->WorkPos.x + 650, main_viewport->WorkPos.y + 20), ImGuiCond_FirstUseEver);
+    // ImGui::SetNextWindowSize(ImVec2(550, 680), ImGuiCond_FirstUseEver);
 
     // Main body of the Demo window starts here.
     if (!ImGui::Begin("Dear ImGui Demo", p_open, window_flags))
